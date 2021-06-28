b63e699b05e476b2f640d4be3301e9da7e08e24a
austin-owensby <austin_owensby@hotmail.com>
2021-06-27 21:48:07 -0500

    2015 day 1 solution

--- Services/2015/Solution2015_01Service.cs
+++ Services/2015/Solution2015_01Service.cs
@@ -1,10 +1,26 @@
+using System;
+using System.IO;
+
 namespace AdventOfCode.Services
 {
     public class Solution2015_01Service: ISolutionDayService{
         public Solution2015_01Service(){}
 
         public string FirstHalf(){
-            return "Solution day 1 part 1";
+            string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs\", "2015_01.txt"));
+
+            int floor = 0;
+
+            foreach(char character in data){
+                if(character == '('){
+                    floor++;
+                }
+                else if(character == ')'){
+                    floor--;
+                }
+            }
+
+            return $"Santa's final floor: {floor}.";
         }
 
         public string SecondHalf(){
