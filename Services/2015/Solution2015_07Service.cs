2d06bfebf93200a112eddb2af1106661d50ca418
Austin Owensby <austin_owensby@hotmail.com>
2021-07-07 23:58:56 -0500

    2015 Day 7 solution

--- Services/2015/Solution2015_07Service.cs
+++ Services/2015/Solution2015_07Service.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -11,67 +12,194 @@ namespace AdventOfCode.Services
         public string FirstHalf(){
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_07.txt"));
             string[] lines = data.Split("\n");
-            List<string> knownRegisters = new();
-            Dictionary<string, ushort> registerValues = new();
+            List<string> knownRegisters = new(){"1"};
+            Dictionary<string, ushort> registerValues = new(){{"1", 1}};
             Dictionary<List<string>, string> unknownRegisters = new();
 
             // Process the data from the file into unknown and known registers
             foreach(string line in lines){
-               string[] instruction = line.Split(" ");
+                string[] instruction = line.Split(" ");
 
                 // # -> a
-               if(instruction.Length == 3){
-                   knownRegisters.Add(instruction[2]);
-                   registerValues[instruction[2]] = UInt16.Parse(instruction[0]);
-               }
+                // a -> b
+                if(instruction.Length == 3){
+
+                    if(UInt16.TryParse(instruction[0], out ushort value)){
+                        knownRegisters.Add(instruction[2]);
+                        registerValues[instruction[2]] = value;
+                    }
+                    else{
+                        unknownRegisters[new List<string>{"ASSIGN", instruction[0]}] = instruction[2];
+                    }
+                }
 
                 // NOT a -> b
-               if(instruction.Length == 4){
-                   unknownRegisters[new List<string>{instruction[0], instruction[1]}] = instruction[3];
-               }
+                if(instruction.Length == 4){
+                    unknownRegisters[new List<string>{instruction[0], instruction[1]}] = instruction[3];
+                }
 
-               // a AND b -> c
-               // 1 AND a -> b
-               // a OR b -> c
-               // a LSHIFT 1 -> b
-               // a RSHIFT 1 -> b
-               if(instruction.Length == 5){
-                   if(instruction[1] == "AND"){
+                // a AND b -> c
+                // 1 AND a -> b
+                // a OR b -> c
+                // a LSHIFT # -> b
+                // a RSHIFT # -> b
+                if(instruction.Length == 5){
+                    if(instruction[1] == "AND"){
                         unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
-                   }
-                   else if(instruction[1] == "OR"){
+                    }
+                    else if(instruction[1] == "OR"){
                         unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
-                   }
-                   else if(instruction[1] == "LSHIFT"){
+                    }
+                    else if(instruction[1] == "LSHIFT"){
                         unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
-                   }
-                   else if(instruction[1] == "RSHIFT"){
-                        unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
-                   }
-               }
-            }
 
-            foreach(KeyValuePair<List<string>, string> pair in unknownRegisters){
-                // Get the arguments for the registers for this instruction
-                List<string> registers = pair.Key.Count == 3 ? new List<string>(){pair.Key[1], pair.Key[2]} : new List<string>{pair.Key[1]};
+                        // If an integer is found, add it as a register
+                        if(UInt16.TryParse(instruction[2], out ushort value)){
+                            if(!knownRegisters.Contains(instruction[2])){
+                                knownRegisters.Add(instruction[2]);
+                                registerValues[instruction[2]] = value;
+                            }
+                        }
+                    }
+                    else if(instruction[1] == "RSHIFT"){
+                        unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
 
-                // If the registers for this instruction are all known, calculate the new value
-                if(knownRegisters.Intersect(registers).Any()){
-
+                        // If an integer is found, add it as a register
+                        if(UInt16.TryParse(instruction[2], out ushort value)){
+                            if(!knownRegisters.Contains(instruction[2])){
+                                knownRegisters.Add(instruction[2]);
+                                registerValues[instruction[2]] = value;
+                            }
+                        }
+                    }
                 }
             }
 
-            return $"";
+            while(!knownRegisters.Contains("a")){
+                foreach(KeyValuePair<List<string>, string> pair in unknownRegisters){
+                    // Get the arguments for the registers for this instruction
+                    List<string> registers = pair.Key.Count == 2 ? new List<string>{pair.Key[1]} : new List<string>{pair.Key[1], pair.Key[2]};
+
+                    // If the registers for this instruction are all known, calculate the new value
+                    if(!registers.Except(knownRegisters).Any()){
+                        registerValues[pair.Value] = (ushort)(pair.Key[0] switch {
+                            "ASSIGN" =>  registerValues[registers[0]],
+                            "NOT" => ~registerValues[registers[0]],
+                            "AND" => registerValues[registers[0]] & registerValues[registers[1]],
+                            "OR" => registerValues[registers[0]] | registerValues[registers[1]],
+                            "LSHIFT" => registerValues[registers[0]] << registerValues[registers[1]],
+                            "RSHIFT" => registerValues[registers[0]] >> registerValues[registers[1]]
+                        });
+
+                        knownRegisters.Add(pair.Value);
+                    }
+                }
+
+                // Remove the known registers from the unknownRegisters
+                unknownRegisters = unknownRegisters.Where(r => !knownRegisters.Contains(r.Value)).ToDictionary(r => r.Key, r => r.Value);
+            }
+
+            return $"Wire a's value is {registerValues["a"]}.";
         }
 
         public string SecondHalf(){            
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_07.txt"));
+            string[] lines = data.Split("\n");
+            List<string> knownRegisters = new(){"1"};
+            Dictionary<string, ushort> registerValues = new(){{"1", 1}};
+            Dictionary<List<string>, string> unknownRegisters = new();
 
-            foreach(char character in data){
+            // Process the data from the file into unknown and known registers
+            foreach(string line in lines){
+                string[] instruction = line.Split(" ");
 
+                // # -> a
+                // a -> b
+                if(instruction.Length == 3){
+
+                    if(UInt16.TryParse(instruction[0], out ushort value)){
+                        knownRegisters.Add(instruction[2]);
+                        registerValues[instruction[2]] = value;
+                    }
+                    else{
+                        unknownRegisters[new List<string>{"ASSIGN", instruction[0]}] = instruction[2];
+                    }
+                }
+
+                // NOT a -> b
+                if(instruction.Length == 4){
+                    unknownRegisters[new List<string>{instruction[0], instruction[1]}] = instruction[3];
+                }
+
+                // a AND b -> c
+                // 1 AND a -> b
+                // a OR b -> c
+                // a LSHIFT # -> b
+                // a RSHIFT # -> b
+                if(instruction.Length == 5){
+                    if(instruction[1] == "AND"){
+                        unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
+                    }
+                    else if(instruction[1] == "OR"){
+                        unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
+                    }
+                    else if(instruction[1] == "LSHIFT"){
+                        unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
+
+                        // If an integer is found, add it as a register
+                        if(UInt16.TryParse(instruction[2], out ushort value)){
+                            if(!knownRegisters.Contains(instruction[2])){
+                                knownRegisters.Add(instruction[2]);
+                                registerValues[instruction[2]] = value;
+                            }
+                        }
+                    }
+                    else if(instruction[1] == "RSHIFT"){
+                        unknownRegisters[new List<string>{instruction[1], instruction[0], instruction[2]}] = instruction[4];
+
+                        // If an integer is found, add it as a register
+                        if(UInt16.TryParse(instruction[2], out ushort value)){
+                            if(!knownRegisters.Contains(instruction[2])){
+                                knownRegisters.Add(instruction[2]);
+                                registerValues[instruction[2]] = value;
+                            }
+                        }
+                    }
+                }
             }
 
-            return $"";
+            // Use the value from the first half and set it to b
+            string[] response = FirstHalf().Split(" ");
+            string valueString = response[response.Length - 1];
+            string valueStringNoPeriod = valueString.Remove(valueString.Length - 1, 1);
+            ushort calculatedValue = UInt16.Parse(valueStringNoPeriod);
+            registerValues["b"] = calculatedValue;
+
+            while(!knownRegisters.Contains("a")){
+                foreach(KeyValuePair<List<string>, string> pair in unknownRegisters){
+                    // Get the arguments for the registers for this instruction
+                    List<string> registers = pair.Key.Count == 2 ? new List<string>{pair.Key[1]} : new List<string>{pair.Key[1], pair.Key[2]};
+
+                    // If the registers for this instruction are all known, calculate the new value
+                    if(!registers.Except(knownRegisters).Any()){
+                        registerValues[pair.Value] = (ushort)(pair.Key[0] switch {
+                            "ASSIGN" =>  registerValues[registers[0]],
+                            "NOT" => ~registerValues[registers[0]],
+                            "AND" => registerValues[registers[0]] & registerValues[registers[1]],
+                            "OR" => registerValues[registers[0]] | registerValues[registers[1]],
+                            "LSHIFT" => registerValues[registers[0]] << registerValues[registers[1]],
+                            "RSHIFT" => registerValues[registers[0]] >> registerValues[registers[1]]
+                        });
+
+                        knownRegisters.Add(pair.Value);
+                    }
+                }
+
+                // Remove the known registers from the unknownRegisters
+                unknownRegisters = unknownRegisters.Where(r => !knownRegisters.Contains(r.Value)).ToDictionary(r => r.Key, r => r.Value);
+            }
+
+            return $"Wire a's value is {registerValues["a"]} after setting b to a's value from part 1.";
         }
     }
 }
