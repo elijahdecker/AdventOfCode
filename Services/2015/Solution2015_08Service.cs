b28e12d976da144b5fbf3a0f80593fcd1059f13d
Austin Owensby <austin_owensby@hotmail.com>
2021-07-08 22:43:07 -0500

    2015 Day 8 solutions

--- Services/2015/Solution2015_08Service.cs
+++ Services/2015/Solution2015_08Service.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Services
 {
@@ -10,21 +11,24 @@ namespace AdventOfCode.Services
         public string FirstHalf(){
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_08.txt"));
 
-            foreach(char character in data){
-                
-            }
+            int charDifference = 0;
 
-            return $"";
+            charDifference += 2 * Regex.Matches(data, "\n").Count; // Each newline means 2 extra single quotes in the code
+            charDifference += Regex.Matches(data, "(\\\\\")|(\\\\\\\\)").Count; // Each \" and \\ represents 1 extra character in the code
+            charDifference += 3 * Regex.Matches(data, "(\\\\)(x)([0-9a-f])([0-9a-f])").Count; // Each \x represents 3 extra characters in the code
+
+            return $"The number of characters of code for string literals minus the number of characters in memory for the values of the strings in total for the entire file is {charDifference}.";
         }
 
         public string SecondHalf(){            
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_08.txt"));
 
-            foreach(char character in data){
+            int charDifference = 0;
 
-            }
+            charDifference += 2 * Regex.Matches(data, "\n").Count; // Each newline means 2 extra single quotes in the code
+            charDifference += Regex.Matches(data, "(\")|(\\\\)").Count; // Each \ and " represents 1 extra character in the code
 
-            return $"";
+            return $"The total number of characters to represent the newly encoded strings minus the number of characters of code in each original string literal is {charDifference}.";
         }
     }
 }
