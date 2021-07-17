fd4bc97242d4520e1acc8af719f8c2450845a207
Austin Owensby <austin_owensby@hotmail.com>
2021-07-17 18:14:39 -0500

    2015 day 17

--- Services/2015/Solution2015_17Service.cs
+++ Services/2015/Solution2015_17Service.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System;
 using System.IO;
 
@@ -9,21 +11,60 @@ namespace AdventOfCode.Services
         public string FirstHalf(){
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_17.txt"));
 
-            foreach(char character in data){
-                
+            List<int> containers = data.Split("\n").SkipLast(1).Select(d => int.Parse(d)).OrderBy(d => d).ToList();
+            int targetTotal = 150;
+            int totalCombinations = 0;
+
+            for(int i = 0; i < Math.Pow(2, 20); i++){
+                int continerSum = 0;
+
+                for(int j = 0; j < containers.Count; j++){
+                    int binaryPower = (int)Math.Pow(2, j);
+                    if((i & binaryPower) == binaryPower){
+                        continerSum += containers[j];
+                    }
+                }
+
+                if(continerSum == targetTotal){
+                    totalCombinations++;
+                }
             }
 
-            return $"";
+            return $"{totalCombinations} combinations of continers can fit {targetTotal} liters.";
         }
 
         public string SecondHalf(){            
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_17.txt"));
 
-            foreach(char character in data){
+            List<int> containers = data.Split("\n").SkipLast(1).Select(d => int.Parse(d)).OrderBy(d => d).ToList();
+            int targetTotal = 150;
+            int totalCombinations = 0;
+            int minRequiredContainers = int.MaxValue;
 
+            for(int i = 0; i < Math.Pow(2, 20); i++){
+                int continerSum = 0;
+                int containersUsed = 0;
+
+                for(int j = 0; j < containers.Count; j++){
+                    int binaryPower = (int)Math.Pow(2, j);
+                    if((i & binaryPower) == binaryPower){
+                        continerSum += containers[j];
+                        containersUsed++;
+                    }
+                }
+
+                if(continerSum == targetTotal){
+                    if(containersUsed < minRequiredContainers){
+                        minRequiredContainers = containersUsed;
+                        totalCombinations = 1;
+                    }
+                    else if(containersUsed == minRequiredContainers){
+                        totalCombinations++;
+                    }
+                }
             }
 
-            return $"";
+            return $"{totalCombinations} combinations of continers can fit {targetTotal} liters with the least amount of containers needed.";
         }
     }
 }
