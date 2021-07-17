67afa9f76b318e7763842fbe11c5c1aba7a8143c
Austin Owensby <austin_owensby@hotmail.com>
2021-07-17 16:41:53 -0500

    2015 day 15

--- Services/2015/Solution2015_15Service.cs
+++ Services/2015/Solution2015_15Service.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Services
 {
@@ -9,22 +11,107 @@ namespace AdventOfCode.Services
         public string FirstHalf(){
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_15.txt"));
 
-            foreach(char character in data){
-                
+            string[] lines = data.Split("\n");
+            lines = lines.SkipLast(1).ToArray();
+
+            List<Ingredient> ingredients = new();
+
+            foreach(string line in lines){
+                string[] words = line.Split(" ");
+
+                Ingredient ingredient = new(){
+                    Capacity = int.Parse(words[2].Split(',')[0]),
+                    Durability = int.Parse(words[4].Split(',')[0]),
+                    Flavor = int.Parse(words[6].Split(',')[0]),
+                    Texture = int.Parse(words[8].Split(',')[0]),
+                    Calories = int.Parse(words[10])
+                };
+
+                ingredients.Add(ingredient);
             }
 
-            return $"";
+            int highestScore = 0;
+
+            // Iterate through each of the possibilities of the 3 ingredient totals
+            // The 4th ingredient will always be 100 - the sum of the first 3
+            for(int i = 0; i <= 100; i++){
+                for(int j = 0; j <= 100 - i; j++){
+                    for(int k = 0; k <= 100 - i - j; k++){
+                        int l = 100 - i - j - k;
+
+                        int capactity = Math.Max(0, i * ingredients[0].Capacity + j * ingredients[1].Capacity + k * ingredients[2].Capacity + l * ingredients[3].Capacity);
+                        int durability = Math.Max(0, i * ingredients[0].Durability + j * ingredients[1].Durability + k * ingredients[2].Durability + l * ingredients[3].Durability);
+                        int flavor = Math.Max(0, i * ingredients[0].Flavor + j * ingredients[1].Flavor + k * ingredients[2].Flavor + l * ingredients[3].Flavor);
+                        int texture = Math.Max(0, i * ingredients[0].Texture + j * ingredients[1].Texture + k * ingredients[2].Texture + l * ingredients[3].Texture);
+
+                        int score = capactity * durability * flavor * texture;
+
+                        if(score > highestScore){
+                            highestScore = score;
+                        }
+                    }
+                }
+            }
+
+            return $"The total score of the highest-scoring cookie is {highestScore}.";
         }
 
         public string SecondHalf(){            
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_15.txt"));
 
-            foreach(char character in data){
+            string[] lines = data.Split("\n");
+            lines = lines.SkipLast(1).ToArray();
 
+            List<Ingredient> ingredients = new();
+
+            foreach(string line in lines){
+                string[] words = line.Split(" ");
+
+                Ingredient ingredient = new(){
+                    Capacity = int.Parse(words[2].Split(',')[0]),
+                    Durability = int.Parse(words[4].Split(',')[0]),
+                    Flavor = int.Parse(words[6].Split(',')[0]),
+                    Texture = int.Parse(words[8].Split(',')[0]),
+                    Calories = int.Parse(words[10])
+                };
+
+                ingredients.Add(ingredient);
             }
 
-            return $"";
+            int highestScore = 0;
+
+            // Iterate through each of the possibilities of the 3 ingredient totals
+            // The 4th ingredient will always be 100 - the sum of the first 3
+            for(int i = 0; i <= 100; i++){
+                for(int j = 0; j <= 100 - i; j++){
+                    for(int k = 0; k <= 100 - i - j; k++){
+                        int l = 100 - i - j - k;
+
+                        int capactity = Math.Max(0, i * ingredients[0].Capacity + j * ingredients[1].Capacity + k * ingredients[2].Capacity + l * ingredients[3].Capacity);
+                        int durability = Math.Max(0, i * ingredients[0].Durability + j * ingredients[1].Durability + k * ingredients[2].Durability + l * ingredients[3].Durability);
+                        int flavor = Math.Max(0, i * ingredients[0].Flavor + j * ingredients[1].Flavor + k * ingredients[2].Flavor + l * ingredients[3].Flavor);
+                        int texture = Math.Max(0, i * ingredients[0].Texture + j * ingredients[1].Texture + k * ingredients[2].Texture + l * ingredients[3].Texture);
+                        int calories = Math.Max(0, i * ingredients[0].Calories + j * ingredients[1].Calories + k * ingredients[2].Calories + l * ingredients[3].Calories);
+
+                        int score = capactity * durability * flavor * texture;
+
+                        if(calories == 500 && score > highestScore){
+                            highestScore = score;
+                        }
+                    }
+                }
+            }
+
+            return $"The total score of the highest-scoring cookie with exactly 500 calories is {highestScore}.";
         }
+    }
+
+    public class Ingredient{
+        public int Capacity {get; set;}
+        public int Durability {get; set;}
+        public int Flavor {get; set;}
+        public int Texture {get; set;}
+        public int Calories {get; set;}
     }
 }
                         
