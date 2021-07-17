9e78e63a009f5b04df2325ede418a7d834fefc4b
Austin Owensby <austin_owensby@hotmail.com>
2021-07-17 10:35:05 -0500

    2015 day 12 part 2

--- Services/2015/Solution2015_12Service.cs
+++ Services/2015/Solution2015_12Service.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -31,12 +30,80 @@ namespace AdventOfCode.Services
         public string SecondHalf(){            
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_12.txt"));
 
-            foreach(char character in data){
+            Stack<ParsedData> stack = new();
+            ParsedData currentObject = new();
+            List<char> validNumberCharacters = new(){'-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            string trackedNumber = "";
+            string trackedRed = "";
 
+            foreach(char character in data){
+                // Track numbers and add them to the existing object when the number is completed
+                if(validNumberCharacters.Contains(character)){
+                    trackedNumber += character;
+                }
+                else if(trackedNumber != ""){
+                    if(!currentObject.HasRed){
+                        currentObject.Sum += int.Parse(trackedNumber);
+                    }
+                    trackedNumber = "";
+                }
+
+                // If a new object/array is found, push the current one to the stack and reset the current object
+                switch(character){
+                    case '{':
+                        // If the parent of this object has red, ignore any children
+                        stack.Push(currentObject);
+                        currentObject = new(){HasRed = currentObject.HasRed};
+                        trackedRed = "";
+                        break;
+                    case '[':
+                        // If the parent of this object has red, so do its children
+                        stack.Push(currentObject);
+                        currentObject = new(){IsArray = true, HasRed = currentObject.HasRed};
+                        trackedRed = "";
+                        break;
+                    case ']':
+                    case '}':
+                        int sum = currentObject.Sum;
+                        currentObject = stack.Pop();
+                        currentObject.Sum += sum;
+                        trackedRed = "";
+                        break;
+                    case 'r':
+                        trackedRed = "r";
+                        break;
+                    case 'e':
+                        if(trackedRed == "r"){
+                            trackedRed = "re";
+                        }
+                        else{
+                            trackedRed = "";
+                        }
+                        break;
+                    case 'd':
+                        if(trackedRed == "re"){
+                            // We only ignore the sum if red is in an object or has a red parent
+                            if(!currentObject.IsArray){
+                                currentObject.HasRed = true;
+                                currentObject.Sum = 0;
+                            }
+                        }
+                        trackedRed = "";
+                        break;
+                    default:
+                        trackedRed = "";
+                        break;
+                }
             }
 
-            return $"";
+            return $"The sum of all the numbers in the document after fixing the red double count is {currentObject.Sum}.";
         }
+    }
+
+    public class ParsedData {
+        public bool IsArray {get; set;}
+        public int Sum {get; set;}
+        public bool HasRed {get; set;}
     }
 }
                         
