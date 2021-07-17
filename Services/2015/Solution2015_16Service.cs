6f67a96c5ecc4efc867bf5cef5478a8a5915c1b6
Austin Owensby <austin_owensby@hotmail.com>
2021-07-17 17:29:10 -0500

    2015 day 16

--- Services/2015/Solution2015_16Service.cs
+++ Services/2015/Solution2015_16Service.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System;
 using System.IO;
 
@@ -9,21 +11,132 @@ namespace AdventOfCode.Services
         public string FirstHalf(){
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_16.txt"));
 
-            foreach(char character in data){
-                
+            string[] lines = data.Split("\n").SkipLast(1).ToArray();
+
+            Dictionary<string, int> mfcsam = new(){
+                {"children:", 3},
+                {"cats:", 7},
+                {"samoyeds:", 2},
+                {"pomeranians:", 3},
+                {"akitas:", 0},
+                {"vizslas:", 0},
+                {"goldfish:", 5},
+                {"trees:", 3},
+                {"cars:", 2},
+                {"perfumes:", 1}
+            };
+
+            int correctAunt = 0;
+
+            foreach(string line in lines){
+                string[] words = line.Split(" ");
+
+                string word = words[2];
+                int value = int.Parse(words[3].Split(",")[0]);
+
+                if(mfcsam[word] == value){
+                    word = words[4];
+                    value = int.Parse(words[5].Split(",")[0]);
+
+                    if(mfcsam[word] == value){
+                        word = words[6];
+                        value = int.Parse(words[7]);
+
+                        if(mfcsam[word] == value){
+                            correctAunt = int.Parse(words[1].Split(":")[0]);
+                        }
+                    }
+                }
             }
 
-            return $"";
+            return $"Aunt Sue {correctAunt} got you the gift.";
         }
 
         public string SecondHalf(){            
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_16.txt"));
 
-            foreach(char character in data){
+            string[] lines = data.Split("\n").SkipLast(1).ToArray();
 
+            Dictionary<string, int> mfcsam = new(){
+                {"children:", 3},
+                {"cats:", 7},
+                {"samoyeds:", 2},
+                {"pomeranians:", 3},
+                {"akitas:", 0},
+                {"vizslas:", 0},
+                {"goldfish:", 5},
+                {"trees:", 3},
+                {"cars:", 2},
+                {"perfumes:", 1}
+            };
+
+            int correctAunt = 0;
+
+            foreach(string line in lines){
+                string[] words = line.Split(" ");
+
+                string word = words[2];
+                int value = int.Parse(words[3].Split(",")[0]);
+
+                if(word == "cats:" || word == "tress:"){
+                    if(mfcsam[word] >= value){
+                        continue;
+                    }
+                }
+                else if(word == "pomeranians:" || word == "goldfish:"){
+                    if(mfcsam[word] <= value){
+                        continue;
+                    }
+                }
+                else{
+                    if(mfcsam[word] != value){
+                        continue;
+                    }
+                }
+                
+                word = words[4];
+                value = int.Parse(words[5].Split(",")[0]);
+
+                if(word == "cats:" || word == "tress:"){
+                    if(mfcsam[word] >= value){
+                        continue;
+                    }
+                }
+                else if(word == "pomeranians:" || word == "goldfish:"){
+                    if(mfcsam[word] <= value){
+                        continue;
+                    }
+                }
+                else{
+                    if(mfcsam[word] != value){
+                        continue;
+                    }
+                }
+
+                    
+                word = words[6];
+                value = int.Parse(words[7]);
+
+                if(word == "cats:" || word == "tress:"){
+                    if(mfcsam[word] >= value){
+                        continue;
+                    }
+                }
+                else if(word == "pomeranians:" || word == "goldfish:"){
+                    if(mfcsam[word] <= value){
+                        continue;
+                    }
+                }
+                else{
+                    if(mfcsam[word] != value){
+                        continue;
+                    }
+                }
+
+                correctAunt = int.Parse(words[1].Split(":")[0]);
             }
 
-            return $"";
+            return $"Aunt Sue {correctAunt} got you the gift.";
         }
     }
 }
