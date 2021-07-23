e079d497516c6307e0759e243b2cb2bc3f87895f
Austin Owensby <austin_owensby@hotmail.com>
2021-07-22 22:58:01 -0500

    2015 day 20

--- Services/2015/Solution2015_20Service.cs
+++ Services/2015/Solution2015_20Service.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System;
 using System.IO;
 
@@ -9,21 +10,77 @@ namespace AdventOfCode.Services
         public string FirstHalf(){
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_20.txt"));
 
-            foreach(char character in data){
-                
+            int goalNumber = int.Parse(data.Split("\n")[0]);
+            int foundHouse = 0;
+
+            for(int currentHouse = 1; currentHouse <= goalNumber / 10; currentHouse++){
+                int sumOfFactors = 0;
+
+                for(int i = 1; i <= Math.Sqrt(currentHouse); i++){
+                    if(currentHouse % i == 0){
+                        sumOfFactors += i;
+
+                        if(currentHouse / i != i){
+                            sumOfFactors += (currentHouse / i);
+                        }
+                    }
+                }
+
+                if(sumOfFactors >= goalNumber / 10){
+                    foundHouse = currentHouse;
+                    break;
+                }
             }
 
-            return $"";
+            return $"The lowest house number of the house to get at least {goalNumber} presents is {foundHouse}.";
         }
 
         public string SecondHalf(){            
             string data =  File.ReadAllText(Path.Combine(Environment.CurrentDirectory, @"Inputs", "2015_20.txt"));
 
-            foreach(char character in data){
+            int goalNumber = int.Parse(data.Split("\n")[0]);
+            int foundHouse = 0;
 
+            Dictionary<int, int> factors = new();
+
+            for(int currentHouse = 1; currentHouse <= goalNumber / 11; currentHouse++){
+                int sumOfFactors = 0;
+
+                for(int i = 1; i <= Math.Sqrt(currentHouse); i++){
+                    if(currentHouse % i == 0){
+                        if(factors.ContainsKey(i)){
+                            if(factors[i] < 50){
+                                factors[i] = 1 + factors[i];
+                                sumOfFactors += i;
+                            }
+                        }
+                        else{
+                            factors[i] = 1;
+                            sumOfFactors += i;
+                        }
+
+                        if(currentHouse / i != i){
+                            if(factors.ContainsKey(currentHouse / i)){
+                                if(factors[currentHouse / i] < 50){
+                                    factors[currentHouse / i] = 1 + factors[currentHouse / i];
+                                    sumOfFactors += (currentHouse / i);
+                                }
+                            }
+                            else{
+                                factors[currentHouse / i] = 1;
+                                sumOfFactors += (currentHouse / i);
+                            }
+                        }
+                    }
+                }
+
+                if(sumOfFactors >= goalNumber / 11){
+                    foundHouse = currentHouse;
+                    break;
+                }
             }
 
-            return $"";
+            return $"The lowest house number of the house to get at least {goalNumber} presents is {foundHouse}.";
         }
     }
 }
