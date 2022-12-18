2072fd984432209dbd23fb4b8956a5c985815360
Austin Owensby <austin_owensby@hotmail.com>
2022-12-17 22:59:28 -0600

    2022 day 16

--- Services/2022/Solution2022_16Service.cs
+++ Services/2022/Solution2022_16Service.cs
@@ -1,19 +1,178 @@
 namespace AdventOfCode.Services
 {
+    public class Valve {
+        public string Name {get; set;}
+        public int FlowRate {get; set;}
+        public List<string> ConnectedValves {get; set;} = new();
+    }
+
     public class Solution2022_16Service : ISolutionDayService
     {
         public Solution2022_16Service() { }
+
+        private List<Valve> Valves {get; set;} = new();
+        private Dictionary<string, int> Distances {get; set;} = new();
+
+        /// <summary>
+        /// Find the distance betwwen 2 valves by checking the starting valves reach and then increasing that reach 1 distance at a time until the end valve is found
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        private int GetValveDistance(string start, string end) {
+            List<string> valveReach = new(){
+                start,
+            };
+
+            int distance = 0;
+
+            while (!valveReach.Contains(end)) {
+                distance++;
+                
+                // Get all connected valves at one more distance
+                List<string> newReach = Valves.Where(v => valveReach.Contains(v.Name)).SelectMany(v => v.ConnectedValves).ToList();
+                valveReach.AddRange(newReach);
+                valveReach = valveReach.Distinct().ToList();
+            }
+
+            return distance;
+        }
+
+        /// <summary>
+        /// Given a starting valve, time left, and available valves calculate the max amount of released pressure
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="availableValves"></param>
+        /// <param name="timeLeft"></param>
+        /// <returns></returns>
+        private int GetPressureReleased(string start, List<string> availableValves, int timeLeft) {
+            int maxPressureReleased = availableValves.Max(valve => {
+                int pressureReleased = 0;
+
+                List<string> keys = new() {start, valve};
+                string key = string.Join(" ", keys.Order());
+                int distance = Distances[key];
+
+                int newTimeLeft = timeLeft - distance - 1;
+
+                if (newTimeLeft > 0) {
+                    int rate = Valves.First(v => v.Name == valve).FlowRate;
+                    pressureReleased += newTimeLeft * rate;
+                    List<string> newAvailableValves = availableValves.Where(v => v != valve).ToList();
+
+                    if (newAvailableValves.Any()) {
+                        pressureReleased += GetPressureReleased(valve, newAvailableValves, newTimeLeft);
+                    }
+                }
+
+                return pressureReleased;
+            });
+
+            return maxPressureReleased;
+        }
+
+        private int GetPressureReleasedWithElephant(string start, string elephantStart, List<string> availableValves, int timeLeft, int elephantTimeLeft) {          
+            int maxPressureReleased = availableValves.Max(valve => {
+                int pressureReleased = 0;
+
+                if (timeLeft > elephantTimeLeft) {
+                    // Our turn
+
+                    // Handle move
+                    List<string> keys = new() {start, valve};
+                    string key = string.Join(" ", keys.Order());
+                    int distance = Distances[key];
+
+                    int newTimeLeft = timeLeft - distance - 1;
+
+                    // Only make the step if there's enough time
+                    List<string> newAvailableValves = availableValves.Where(v => v != valve).ToList();
+
+                    int minDistance = newAvailableValves.Any() ? newAvailableValves.Min(v => {
+                        List<string> keys = new() {valve, v};
+                        string key = string.Join(" ", keys.Order());
+                        return Distances[key];
+                    }) : 0; 
+
+                    int rate = Valves.First(v => v.Name == valve).FlowRate;
+
+                    if (newTimeLeft > 0) {
+                        // Take our step
+                        pressureReleased += newTimeLeft * rate;
+
+                        // If we or the elephant can make another step
+                        if (newAvailableValves.Any() && (newTimeLeft > minDistance || elephantTimeLeft > minDistance)) {
+                            pressureReleased += GetPressureReleasedWithElephant(valve, elephantStart, newAvailableValves, newTimeLeft, elephantTimeLeft);
+                        }
+                    }
+                }
+                else {
+                    // Elephant's turn
+
+                    // Handle move
+                    List<string> keys = new() {elephantStart, valve};
+                    string key = string.Join(" ", keys.Order());
+                    int distance = Distances[key];
+
+                    int newTimeLeftElephant = elephantTimeLeft - distance - 1;
+
+                    // Only make the step if there's enough time
+                    List<string> newAvailableValves = availableValves.Where(v => v != valve).ToList();
+
+                    int minDistance = newAvailableValves.Any() ? newAvailableValves.Min(v => {
+                        List<string> keys = new() {valve, v};
+                        string key = string.Join(" ", keys.Order());
+                        return Distances[key];
+                    }) : 0; 
+
+                    int rate = Valves.First(v => v.Name == valve).FlowRate;
+                
+                    if (newTimeLeftElephant > 0) {
+                        // Take the elephant's step
+                        pressureReleased += newTimeLeftElephant * rate;
+
+                        // If we or the elephant can make another step
+                        if (newAvailableValves.Any() && (newTimeLeftElephant > minDistance || timeLeft > minDistance)) {
+                            pressureReleased += GetPressureReleasedWithElephant(start, valve, newAvailableValves, timeLeft, newTimeLeftElephant);
+                        }
+                    }
+                }
+                
+                return pressureReleased;
+            });
+
+            return maxPressureReleased;
+        }
 
         public async Task<string> FirstHalf(bool send)
         {
             List<string> lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Inputs", "2022_16.txt")).ToList();
 
-            int answer = 0;
+            // Parse the file into a list of valves
+            Valves = lines.QuickRegex(@"Valve (.+) has flow rate=(\d+); tunnels? leads? to valves? (.+)").Select(valve => new Valve(){
+                Name = valve[0],
+                FlowRate = int.Parse(valve[1]),
+                ConnectedValves = valve[2].SplitSubstring(", ")
+            }).ToList();
 
-            foreach (string line in lines)
-            {
+            // Find only the valves that are working to visit
+            List<string> availableValves = Valves.Where(v => v.FlowRate > 0).Select(v => v.Name).Order().ToList();
 
+            // Calculate the distances between all working valves + the start
+            List<string> valves = availableValves.Select(n => n).ToList();
+            valves.Add("AA");
+            valves = valves.Order().ToList();
+
+            for (int i = 0; i < valves.Count; i++) {
+                for (int j = i + 1; j < valves.Count; j++) {
+                    string node1 = valves[i];
+                    string node2 = valves[j];
+
+                    Distances.Add($"{node1} {node2}", GetValveDistance(node1, node2));
+                }
             }
+
+            int answer = GetPressureReleased("AA", availableValves, 30);
 
             return await Utility.SubmitAnswer(2022, 16, false, answer, send);
         }
@@ -22,12 +181,33 @@ namespace AdventOfCode.Services
         {
             List<string> lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Inputs", "2022_16.txt")).ToList();
 
-            int answer = 0;
+            // Parse the file into a list of valves
+            Valves = lines.QuickRegex(@"Valve (.+) has flow rate=(\d+); tunnels? leads? to valves? (.+)").Select(valve => new Valve(){
+                Name = valve[0],
+                FlowRate = int.Parse(valve[1]),
+                ConnectedValves = valve[2].SplitSubstring(", ")
+            }).ToList();
 
-            foreach (string line in lines)
-            {
+            // Find only the valves that are working to visit
+            List<string> availableValves = Valves.Where(v => v.FlowRate > 0).Select(v => v.Name).Order().ToList();
 
+            // Calculate the distances between all working valves + the start
+            List<string> valves = availableValves.Select(n => n).ToList();
+            valves.Add("AA");
+            valves = valves.Order().ToList();
+
+            for (int i = 0; i < valves.Count; i++) {
+                for (int j = i + 1; j < valves.Count; j++) {
+                    string node1 = valves[i];
+                    string node2 = valves[j];
+
+                    Distances.Add($"{node1} {node2}", GetValveDistance(node1, node2));
+                }
             }
+
+            int answer = GetPressureReleasedWithElephant("AA", "AA", availableValves, 26, 26);
+
+            // 2549 Too low
 
             return await Utility.SubmitAnswer(2022, 16, true, answer, send);
         }
