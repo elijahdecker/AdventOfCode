74d7dc514153c7f936af2901cdb039616984fdaf
Austin Owensby <austin_owensby@hotmail.com>
2022-12-24 11:13:54 -0600

    2015 day 25

--- Services/2015/Solution2015_25Service.cs
+++ Services/2015/Solution2015_25Service.cs
@@ -8,12 +8,27 @@ namespace AdventOfCode.Services
         {
             List<string> lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "Inputs", "2015_25.txt")).ToList();
 
-            int answer = 0;
+            List<int> values = lines.First().QuickRegex(@"To continue, please consult the code grid in the manual.  Enter the code at row (\d+), column (\d+).").ToInts();
 
-            foreach (string line in lines)
-            {
+            int row = values.First();
+            int column = values.Last();
 
+            int item = Enumerable.Range(1, column).Sum();
+            int step = column;
+
+            for (int i = 1; i < row; i++) {
+                item += step;
+                step++;
             }
+
+            long answer = 20151125;
+
+            for (int i = 0; i < item - 1; i++) {
+                answer = answer * (long)252533 % (long)33554393;
+            }
+
+            // Too high 18367913, 18361853, 17370278
+            
 
             return await Utility.SubmitAnswer(2015, 25, false, answer, send);
         }
