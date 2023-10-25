5f224005d2b1877a3c0ad2edacee528df9633248
Austin Owensby <austin_owensby@hotmail.com>
2023-10-24 20:43:05 -0500

    Add extra logic checks for the controller

--- Controllers/Controller.cs
+++ Controllers/Controller.cs
@@ -29,6 +29,14 @@ namespace AdventOfCode.Controllers
         [HttpGet("run-solution")]
         public async Task<ActionResult<string>> GetSolution([FromQuery, BindRequired] int year = Globals.START_YEAR, [FromQuery, BindRequired] int day = 1, bool secondHalf = false, bool send = false, bool example = false)
         {
+            if (send && example) {
+                return BadRequest("You're attempting to submit your answer to AOC while using an example input, this is likely a mistake.");
+            }
+
+            if (day == 25 && secondHalf) {
+                return NotFound("There is no problem for Day 25 part 2, solve all other problems to get the last star.");
+            }
+
             try
             {
                 return await solutionService.GetSolution(year, day, secondHalf, send, example);
